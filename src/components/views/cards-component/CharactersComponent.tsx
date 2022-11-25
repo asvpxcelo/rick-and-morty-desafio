5ab5660df7b636d560c9dbc105ee65e3b5877392
Marcelo <marcelo.soares@helpper.com.br>
2022-11-25 15:13:17 -0300

    refactor: characters-component

--- src/components/views/cards-component/CharactersComponent.tsx
+++ src/components/views/cards-component/CharactersComponent.tsx
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
-import SearchBarComponent from "../search-bar-component/SearchBarComponent";
 import "./Characters.css";
 
 const CharactersComponent = ({ characters }: any) => {
-  console.log(characters, 'characters');
+  console.log(characters, "characters");
   return (
     <div className="container">
       <div className="characters__row">
@@ -12,8 +11,10 @@ const CharactersComponent = ({ characters }: any) => {
             <div key={index} className="characters__col">
               <img src={item?.image} alt="" className="characters__image" />
               <div className="characters__info">
-                <h3 className="">{item?.name}</h3>
-                <h3 className="">{item?.status}</h3>
+                <h5 className="">Name: {item?.name}</h5>
+                <h5 className="">Status: {item?.status}</h5>
+                <h5 className="">Espécie: {item?.species}</h5>
+                <h5 className="">Gênero: {item?.gender}</h5>
               </div>
             </div>
           );
