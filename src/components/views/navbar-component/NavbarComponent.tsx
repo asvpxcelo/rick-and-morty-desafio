b0590b46e3844f35aa7abacaa9aa93b3b1336136
Marcelo <marcelo.soares@helpper.com.br>
2022-11-25 15:13:36 -0300

    feature: navbar - eslint

--- src/components/views/navbar-component/NavbarComponent.tsx
+++ src/components/views/navbar-component/NavbarComponent.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import './Navbar.css';
+import React from "react";
+import "./Navbar.css";
+import SearchBarComponent from "../search-bar-component/SearchBarComponent";
+
 
 const NavbarComponent = () => {
-    return (
-        <header>
-            <div className='navbar__list'>
-            <h1>Rick And Morty API</h1>
-                <div className='navbar__links'>
-                    <li><a href='#'></a>Episodes</li>
-                    <li><a href='#'></a>Charapcters</li>
-                    <li><a href='#'></a>Location</li>
-                </div>
-            </div>
-        </header>
-    )
-}
+  return (
+    <header>
+      <div className="navbar__list">
+        <h1>Rick And Morty API</h1>
+        <div className="searchbar__component">
+        <SearchBarComponent />
+        </div>
+        <div className="navbar__links">
+          <li>
+            <a href="#"></a>Episodes
+          </li>
+          <li>
+            <a href="#"></a>Charapcters
+          </li>
+          <li>
+            <a href="#"></a>Location
+          </li>
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default NavbarComponent;
