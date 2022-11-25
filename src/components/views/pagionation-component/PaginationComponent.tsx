5efb59596740173c8902075fcc61f8f84ecfbe89
Marcelo <marcelo.soares@helpper.com.br>
2022-11-25 15:13:04 -0300

    feat: pagination-component

--- src/components/views/pagionation-component/PaginationComponent.tsx
+++ src/components/views/pagionation-component/PaginationComponent.tsx
@@ -1,22 +1,24 @@
-import React from 'react';
-import './Pagination.css';
+import React from "react";
+import "./Pagination.css";
 
 const PaginationComponent = ({ setPages }: any) => {
+  const nextPage = () => {
+    setPages((x) => x + 1);
+  };
 
-    const nextPage = () => {
-        setPages((x) => x + 1);
-    }
-
-    const previusPage = () => {
-        setPages((x) => x - 1);
-    }
-    return (
-        <div className='container'>
-            <button className='button__next__page' onClick={nextPage}>Next Page</button>
-            <button className='button__next__page' onClick={previusPage}>Previous Page</button>
-
-        </div>
-    )
-}
+  const previusPage = () => {
+    setPages((x) => x - 1);
+  };
+  return (
+    <div className="container">
+      <button className="button__next__page" onClick={nextPage}>
+        Next Page
+      </button>
+      <button className="button__next__page" onClick={previusPage}>
+        Previous Page
+      </button>
+    </div>
+  );
+};
 
 export default PaginationComponent;
