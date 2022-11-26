b9fe9be7db8d220aa018dc20fed1e6f6d4e45e9f
Marcelo <marcelo.soares@helpper.com.br>
2022-11-26 12:04:10 -0300

    feat: handleChange Event

--- src/components/search-bar-component/SearchBarComponent.tsx
+++ src/components/search-bar-component/SearchBarComponent.tsx
@@ -8,10 +8,14 @@ const SearchBarComponent = () => {
 
     }, []);
 
+    const handleChange = (event: any) => {
+        console.log(event.target.value);
+    }
+
     return (
         <div className='container'>
         <form>
-            <input type="text" placeholder="Search a Guys.."/>
+            <input type="text" placeholder="Search a Guys.." onChange={handleChange}/>
             <button className='button__search'>Search</button>
         </form>
         </div>
