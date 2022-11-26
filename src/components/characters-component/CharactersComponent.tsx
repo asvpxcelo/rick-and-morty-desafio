4497b0c8f44b09a57bac80aee674d930cb82054e
Marcelo <marcelo.soares@helpper.com.br>
2022-11-26 19:51:39 -0300

    feat: characters

--- src/components/characters-component/CharactersComponent.tsx
+++ src/components/characters-component/CharactersComponent.tsx
@@ -11,10 +11,11 @@ const CharactersComponent = (props: { characters: Array<Caracter> } ) => {
             <div key={index} className="characters__images__info">
               <img src={item?.image} alt="" className="characters__image" />
               <div className="characters__info">
-                <h5 className="">Name: {item?.name}</h5>
+                <h5 className="characters__info__name">Name: {item?.name}</h5>
                 <h5 className="characters__status">Status: {item?.status}</h5>
                 <h5 className="">Espécie: {item?.species}</h5>
                 <h5 className="">Gênero: {item?.gender}</h5>
+                <h5 className="characters__location">Ultima Localização: {item?.location?.name}</h5>
               </div>
             </div>
           );
