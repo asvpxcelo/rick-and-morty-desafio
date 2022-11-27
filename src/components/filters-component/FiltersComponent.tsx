13cdbbbb16d748c13893be3e0935f15745249365
Marcelo <marcelo.soares@helpper.com.br>
2022-11-27 00:52:41 -0300

    feat: filter-component

---
+++ src/components/filters-component/FiltersComponent.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface Filters {
+    name: string,
+    index: number,
+    items: [],
+}
+const FiltersComponent = () => {
+    return (
+    <div>
+        <h1>Alo</h1>
+    </div>
+    );
+}
+ 
+export default FiltersComponent;
